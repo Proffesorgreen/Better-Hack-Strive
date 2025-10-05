6f193ae2e0a01f10e07992bd0406ce2e0e375501
KidusHawoltu <kidushawoltu@gmail.com>
2025-10-05 04:01:36 +0300

    Add betterauth

---
+++ webapp_betterhack/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { nextCookies } from "better-auth/next-js";
+const client = new MongoClient("mongodb://ethio-guide:G82W8klR95riD7BT@ac-hdiqau3-shard-00-00.ggdpooh.mongodb.net:27017,ac-hdiqau3-shard-00-01.ggdpooh.mongodb.net:27017,ac-hdiqau3-shard-00-02.ggdpooh.mongodb.net:27017/?ssl=true&replicaSet=atlas-xjj6s0-shard-0&authSource=admin&retryWrites=true&w=majority&appName=EthioGuideCluster");
+const db = client.db();
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        client
+    }),
+        emailAndPassword: { 
+        enabled: true, 
+    }, 
+        socialProviders: { 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    },
+    plugins: [nextCookies()],
+});
